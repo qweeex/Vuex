b3139266a32b34b0ba01fbb072974854937ef58e
AlenaSharkova <alena.s_00@mail.ru>
2023-01-27 14:08:00 +0300

    Initial commit

--- vuex/src/main.js
+++ vuex/src/main.js
@@ -2,18 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
-import {store} from './store'
-import Vue from 'vue'
 
 loadFonts()
 
 createApp(App)
   .use(vuetify)
   .mount('#app')
-
-Vue.config.productionTip = false  
-
-new Vue({
-  store,
-  render: h => h(App),
-});
